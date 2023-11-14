66427850a2e24618c79229143c7fa84ec9b4ffbd
nayzawaung750nza <nayzawaung750@gmail.com>
2023-11-14 09:16:33 +0630

    Prop Types and Validations

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,18 @@
     <greet name="Zaw" heroName="Ant Man"></greet>
     <greet name="Aung" heroName="Super Man"></greet>
     <greet :name="name" :heroName="channel"></greet>
+    <Article title="Article Title" :likes="50" :isPublished="true" />
   </div>
 </template>
 
 <script>
 import Greet from './components/Greet.vue';
+import Article from './components/Article.vue';
 export default {
   name: 'App',
   components: {
     Greet,
+    Article,
   },
   data(){
     return{
---
+++ src/components/Article.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h2>Article Component</h2>
+        <h3>{{ title }} -- Like - {{ likes }} -- Published is {{ isPublished ? 'Yes':'No' }}</h3>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Article',
+        props:{
+            title:{
+                type:String,
+                required:true,
+                default:'Article Default Title'
+            },
+            likes:Number,
+            isPublished:Boolean,
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
